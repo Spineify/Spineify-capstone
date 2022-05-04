be6d5166cb90efea2aceb3b9898fe777050fc622
Olivia Liao <ol64@cornell.edu>
2022-05-04 12:47:50 -0400

    set up electron

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-  entry: [
-    './client/index.js'
-  ],
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-react'
-          ]
-        }
-      }
-    ]
-  }
+	entry: ['./client/index.js'],
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js',
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-react'],
+				},
+			},
+			{
+				test: [/\.s[ac]ss$/i, /\.css$/i],
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+		],
+	},
 }
