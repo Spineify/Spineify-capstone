4ad60a843f12013e002fd868489df328ac1a8ff1
Brooke Stenglein <b.morsekarzen@gmail.com>
2022-05-06 11:56:51 -0500

    actual survey creation page is showing up

--- client/App.js
+++ client/App.js
@@ -1,17 +1,16 @@
-import React from 'react'
-
-import Navbar from './components/Navbar'
-import Routes from './Routes'
-import Posenet from './components/Posenet'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+import Posenet from "./components/Posenet";
 
 const App = () => {
-	return (
-		<div>
-			{/* <Navbar />
-			<Routes /> */}
-			<Posenet />
-		</div>
-	)
-}
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+      {/* <Posenet /> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
