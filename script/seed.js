5351c51293f9594b0a0f5c60d2830f2435a9f466
Jenny Cho <chojenny112@gmail.com>
2022-05-09 11:19:50 -0400

    seed file

--- script/seed.js
+++ script/seed.js
@@ -1,33 +1,52 @@
-'use strict'
+"use strict";
 
-const {db, models: {User, Stretch} } = require('../server/db')
+const {
+  db,
+  models: { User, Stretch },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({ username: "cody", password: "123" }),
+    User.create({ username: "murphy", password: "123" }),
+  ]);
 
   const stretches = await Promise.all([
     //put stretches here. Check data types in db!
-  ])
+    Stretch.create({
+      name: "Neck stretch",
+      directions:
+        "1) Relax in your chair and lean your head forward\n2) Slowly roll toward one side and hold for 10 seconds\n3) Repeat on other side\n4) Relax again and lift your chin back to starting position\n5) Repeat 3 times in each direction",
+      category: ["neck"],
+      imageURL:
+        "https://www.performancehealth.com/media/wysiwyg/blog/articles/shutterstock_464539835.jpg",
+    }),
+    Stretch.create({
+      name: "Latissimus or Overhead Reach stretch",
+      directions:
+        "1) Extend your right arm overhead and reach to the opposite side\n2) Hold for 10 to 30 seconds\n3) Repeat on the other side using your left arm",
+      category: ["shoulders"],
+      imageURL:
+        "https://www.performancehealth.com/media/wysiwyg/blog/articles/shutterstock_1134004811.jpg",
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -36,16 +55,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -55,8 +74,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
