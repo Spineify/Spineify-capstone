a225eef598722de4ab7148a5665ab815f769c791
Olivia Liao <ol64@cornell.edu>
2022-05-10 17:55:50 -0400

    added model to thunk

--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,22 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import auth from "./auth";
-import surveyReducer from "./surveyData";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import auth from './auth'
+import surveyReducer from './surveyData'
+import posesReducer from './posture'
+import modelReducer from './tmModel'
 
-const reducer = combineReducers({ auth, surveyReducer });
+const reducer = combineReducers({
+	auth,
+	surveyReducer,
+	posesReducer,
+	modelReducer,
+})
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
-);
-const store = createStore(reducer, middleware);
+	applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+)
+const store = createStore(reducer, middleware)
 
-export default store;
-export * from "./auth";
+export default store
+export * from './auth'
