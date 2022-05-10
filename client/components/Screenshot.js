91e0b6ea3eee633ad665c71013ddad9d026bafd4
Olivia Liao <ol64@cornell.edu>
2022-05-10 17:49:19 -0400

    added model to thunk creator

--- client/components/Screenshot.js
+++ client/components/Screenshot.js
@@ -1,104 +1,25 @@
-import Webcam from "react-webcam";
-import React, { useRef, useState } from "react";
-//import { useDispatch, useSelector } from "react-redux";
-// import * as tf from '@tensorflow/tfjs'
-// import * as tmPose from '@teachablemachine/pose'
-import { addPose } from "../store/posture";
+import Webcam from 'react-webcam'
+import React, { useRef } from 'react'
 
-function Screenshot({ model, webcamRef }) {
-  //set states/dispatch
-  // const [imageSrc, setImageSrc] = useState('/good_posture.jpeg')
-  const [webcamToggle, setWebcamToggle] = useState(false);
-  // const [model, setModel] = useState({})
-  // const dispatch = useDispatch()
-  // const userId = useSelector((state) => state.auth.id)
+function Screenshot({ webcamRef }) {
+	// const canvasRef = useRef(null)
 
-  //const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-
-  //capture image using react-webcam
-  // const capture = React.useCallback(() => {
-  // 	const imageSrc = webcamRef.current.getScreenshot()
-  // 	setImageSrc(imageSrc)
-  // })
-
-  //teachable machine model
-  // const URL = 'https://teachablemachine.withgoogle.com/models/TzVVyOfnc/'
-
-  // const init = async () => {
-  // 	const modelURL = URL + 'model.json'
-  // 	const metadataURL = URL + 'metadata.json'
-
-  // 	setModel(await tmPose.load(modelURL, metadataURL))
-  // 	// const maxPredictions = model.getTotalClasses()
-  // 	console.log('initialize model: ', model)
-  // }
-
-  // const predict = async () => {
-  // 	const imageElement = document.getElementById('image')
-  // 	const flipHorizontal = false
-  // 	const { pose, posenetOutput } = await model.estimatePose(
-  // 		imageElement,
-  // 		flipHorizontal
-  // 	)
-  // 	const prediction = await model.predict(posenetOutput)
-  // 	console.log('prediction:', prediction)
-
-  // 	dispatch(addPose({ id: userId, data: prediction }))
-  // }
-
-  return (
-    <div className="screenshot">
-      {/* <button onClick={() => init()}>Start</button> */}
-      <div>
-        {/* <img
-					id="image"
-					style={{ width: 400, height: 300, zindex: 10 }}
-					src={imageSrc}
-				/> */}
-        {/* {webcamToggle ? ( */}
-        <div>
-          <Webcam
-            ref={webcamRef}
-            screenshotFormat="image/png"
-            style={{
-              // position: 'absolute',
-              // marginLeft: 'auto',
-              // marginRight: 'auto',
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 400,
-              height: 400,
-              opacity: 0,
-            }}
-          />
-          {/* <button
-              onClick={(e) => {
-                e.preventDefault();
-                capture();
-                setWebcamToggle(false);
-                predict();
-              }}
-            >
-              Take Picture
-            </button> */}
-        </div>
-        {/* ) : (
-          <div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setWebcamToggle(true);
-              }}
-            >
-              Take Screenshot
-            </button>
-          </div>
-        )}
-      </div> */}
-        {/* <canvas
+	return (
+		<div className="screenshot">
+			<Webcam
+				ref={webcamRef}
+				screenshotFormat="image/png"
+				style={{
+					left: 0,
+					right: 0,
+					textAlign: 'center',
+					zindex: 9,
+					width: 400,
+					height: 400,
+					opacity: 0,
+				}}
+			/>
+			{/* <canvas
 				ref={canvasRef}
 				style={{
 					position: 'absolute',
@@ -112,9 +33,8 @@ function Screenshot({ model, webcamRef }) {
 					height: 400,
 				}}
 			/> */}
-      </div>
-    </div>
-  );
+		</div>
+	)
 }
 
-export default Screenshot;
+export default Screenshot
--- client/components/Tracker.js
+++ client/components/Tracker.js
@@ -1,133 +1,97 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Screenshot from "./Screenshot";
-import { addPose } from "../store/posture";
-import Webcam from "react-webcam";
+import React, { useState, useRef, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Screenshot from './Screenshot'
+import { addPose } from '../store/posture'
+import Webcam from 'react-webcam'
 
-const GOOD_POSTURE = "/good_posture.jpeg";
+const GOOD_POSTURE = '/good_posture.jpeg'
 function Tracker() {
-  const [start, setStart] = useState(false);
-  const [imageSrc, setImageSrc] = useState(GOOD_POSTURE);
-  const [model, setModel] = useState(null);
+	const [start, setStart] = useState(false)
+	const [imageSrc, setImageSrc] = useState(GOOD_POSTURE)
+	const model = useSelector((state) => state.modelReducer)
 
-  const dispatch = useDispatch();
-  const webcamRef = useRef(null);
-  const pictureRef = useRef(null);
+	const dispatch = useDispatch()
+	const webcamRef = useRef(null)
+	const pictureRef = useRef(null)
 
-  // useEffect(() => dispatch(modelThunk), []); //empty array is CDM, only runs once // init in root
+	// useEffect(() => dispatch(modelThunk), []); //empty array is CDM, only runs once // init in root
 
-  // useEffect(() => dispatch(analyzeThunk),[imageSrc]) //where there are dependencies, its like CDU, runs everytime dependency changes
+	useEffect(() => {
+		if (!model) {
+			return
+		}
+		const captureInterval = setInterval(function () {
+			//if there is no active webcam, it wont take screenshot
+			if (!webcamRef.current) {
+				return
+			}
+			const imageSrc = webcamRef.current.getScreenshot()
+			setImageSrc(imageSrc)
+		}, 5000)
+		return () => {
+			//runs when you leave the page
+			clearInterval(captureInterval)
+		}
+	}, [model])
 
-  useEffect(() => {
-    const URL = "https://teachablemachine.withgoogle.com/models/TzVVyOfnc/";
-    const init = async () => {
-      const modelURL = URL + "model.json";
-      const metadataURL = URL + "metadata.json";
+	useEffect(() => {
+		if (start && imageSrc !== GOOD_POSTURE) {
+			analyze()
+		}
+		if (!start && imageSrc !== GOOD_POSTURE) {
+			setImageSrc(GOOD_POSTURE)
+		}
+	}, [imageSrc, start])
 
-      const loadedModel = await tmPose.load(modelURL, metadataURL);
-      setModel(loadedModel);
+	const analyze = async () => {
+		let imageElement = pictureRef.current
+		console.log(imageElement)
+		const flipHorizontal = false
+		let { posenetOutput } = await model.estimatePose(
+			imageElement,
+			flipHorizontal
+		)
+		let prediction = await model.predict(posenetOutput)
+		console.log('prediction:', prediction)
+		dispatch(addPose({ data: prediction }))
+	}
 
-      console.log("initialize model: ", model);
-    };
-    init();
-  }, []);
-
-  console.log("rendering start", start);
-  useEffect(() => {
-    if (!model) {
-      return;
-    }
-    const captureInterval = setInterval(function () {
-      //console.log("about to capture", start);
-      //if there is no active webcam, it wont take screenshot
-      if (!webcamRef.current) {
-        return;
-      }
-      const imageSrc = webcamRef.current.getScreenshot();
-      setImageSrc(imageSrc);
-    }, 5000);
-    return () => {
-      //runs when you leave the page
-      clearInterval(captureInterval);
-    };
-  }, [model]);
-
-  useEffect(() => {
-    // console.log("image source", imageSrc);
-    // console.log("pictureRef", pictureRef.current);
-    // console.log("start", start);
-    if (start && imageSrc !== GOOD_POSTURE) {
-      analyze();
-    }
-    if (!start && imageSrc !== GOOD_POSTURE) {
-      setImageSrc(GOOD_POSTURE);
-    }
-  }, [imageSrc, start]);
-
-  //init in a thunk, set model in store --> thunk should be dispatched in a useEffect, use through useSelector
-
-  //another useEffect for setInterval, function that returns a function to end
-  // const startPoses = () => {
-  //   setIntervalId(
-  //     setInterval(function () {
-  //       capture();
-  //       // analyze();
-  //     }, 5000)
-  //   );
-  // };
-
-  //analyze should also be a thunk and should take pictureRef.current, and model as parameters. dispatched in useEffect
-  const analyze = async () => {
-    let imageElement = pictureRef.current;
-    console.log("Current image Element", imageElement);
-    const flipHorizontal = false;
-    let { pose, posenetOutput } = await model.estimatePose(
-      imageElement,
-      flipHorizontal
-    );
-    //console.log("postNet OUTPUT", posenetOutput);
-    let prediction = await model.predict(posenetOutput);
-    console.log("prediction:", prediction);
-
-    dispatch(addPose({ data: prediction }));
-  };
-
-  return (
-    <div className="screenshot">
-      {start ? (
-        <div>
-          <Screenshot model={model} webcamRef={webcamRef} />
-        </div>
-      ) : null}
-      <div>
-        <img
-          ref={pictureRef}
-          id="image"
-          style={{
-            width: 400,
-            height: 300,
-            zindex: 10,
-            ...(start && { display: "none" }),
-          }}
-          src={imageSrc}
-        />
-      </div>
-      <button
-        onClick={() => {
-          setStart(true);
-        }}
-      >
-        Start
-      </button>
-      <button
-        onClick={() => {
-          setStart(false);
-        }}
-      >
-        Stop
-      </button>
-    </div>
-  );
+	return (
+		<div className="screenshot">
+			{start ? (
+				<div>
+					<Screenshot model={model} webcamRef={webcamRef} />
+				</div>
+			) : null}
+			<div>
+				<img
+					ref={pictureRef}
+					id="image"
+					style={{
+						width: 400,
+						height: 300,
+						zindex: 10,
+						...(start && { display: 'none' }),
+					}}
+					src={imageSrc}
+				/>
+			</div>
+			<button
+				onClick={() => {
+					setStart(true)
+				}}
+			>
+				Start
+			</button>
+			<button
+				onClick={() => {
+					setStart(false)
+				}}
+			>
+				Stop
+			</button>
+		</div>
+	)
 }
 
-export default Tracker;
+export default Tracker
