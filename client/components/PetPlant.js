87fab42317b3bb7d98d180bd982430b9904be4c7
Jenny Cho <chojenny112@gmail.com>
2022-05-13 17:49:40 -0400

    cron function and draggable

--- client/components/PetPlant.js
+++ client/components/PetPlant.js
@@ -1,87 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const PetPlant = () => {
-	const [chest, setChest] = useState('closed')
-	//plant data
-	const plant = useSelector((state) => state.plantReducer)
-	const { inventory } = plant
-	const [water, setWater] = useState(inventory.water)
-	const [nutritiousWater, setNutritiousWater] = useState(
-		inventory.nutritiousWater
-	)
-	const [fertilizer, setFertilizer] = useState(inventory.fertilizer)
+  const [chest, setChest] = useState("closed");
+  //plant data
+  const plant = useSelector((state) => state.plantReducer);
+  const { inventory } = plant;
+  const [draggedItem, setDraggedItem] = useState("");
+  const [totalPoints, setTotalPoints] = useState(plant.points);
 
-	//add setInterval to dispatch(getPlant())
-	//cron job nodejs to schedule and handle notification
-	//add notification when received reward
+  //add setInterval to dispatch(getPlant())
+  //cron job nodejs to schedule and handle notification
+  //add notification when received reward
+  const onDrag = (event) => {
+    event.preventDefault();
+    setDraggedItem(event.target.value);
+  };
 
-	const level = String(plant.level)
-	const points = String(plant.points)
-	const date = new Date()
-	const hour = date.getHours()
-	let timeOfDay
-	if (hour > 6 && hour <= 10) {
-		timeOfDay = 'morning'
-	} else if (hour > 10 && hour <= 17) {
-		timeOfDay = 'afternoon'
-	} else {
-		timeOfDay = 'evening'
-	}
+  const onDrop = (event) => {
+    if (draggedItem === "fertilizer") {
+      setTotalPoints(totalPoints + 3);
+    } else if (draggedItem === "nutritiousWater") {
+      setTotalPoints(totalPoints + 2);
+    } else if (draggedItem === "water") {
+      setTotalPoints(totalPoints + 1);
+    }
+  };
 
-	const toggleChest = () => {
-		if (chest === 'opened') {
-			setChest('closed')
-		} else {
-			setChest('opened')
-		}
-	}
+  const level = String(plant.level);
+  const points = String(plant.points);
+  const date = new Date();
+  const hour = date.getHours();
+  let timeOfDay;
+  if (hour > 6 && hour <= 10) {
+    timeOfDay = "morning";
+  } else if (hour > 10 && hour <= 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "evening";
+  }
 
-	return (
-		<div className={`gameFrame ${timeOfDay}`}>
-			<div className="progress">
-				<div
-					className="progress-bar progress-bar-striped progress-bar-animated"
-					role="progressbar"
-					aria-valuenow={`${points}`}
-					aria-valuemin="0"
-					aria-valuemax="12"
-					style={{ width: `${Math.floor((points / 12) * 100)}%` }}
-				></div>
-			</div>
-			<div className="game">
-				<img className="tree" src={`./gamification/tree_${level}.png`} />
-				<div className="inventory">
-					<img
-						className="chest"
-						onClick={() => toggleChest()}
-						src={`./gamification/chest_${chest}.png`}
-					/>
-					{chest === 'opened' ? (
-						<div className="rewards">
-							<div className="item">
-								<img className="img" src={'./gamification/dirt.png'} />
-								<span>{`${inventory.fertilizer}`}</span>
-							</div>
+  const toggleChest = () => {
+    if (chest === "opened") {
+      setChest("closed");
+    } else {
+      setChest("opened");
+    }
+  };
 
-							<div className="item">
-								<img
-									className="img"
-									src={'./gamification/nutritious_water.png'}
-								/>
-								<span>{`${inventory.nutritiousWater}`}</span>
-							</div>
+  return (
+    <div className={`gameFrame ${timeOfDay}`}>
+      <div className="progress">
+        <div
+          className="progress-bar progress-bar-striped progress-bar-animated"
+          role="progressbar"
+          aria-valuenow={`${points}`}
+          aria-valuemin="0"
+          aria-valuemax="12"
+          style={{ width: `${Math.floor((points / 12) * 100)}%` }}
+        ></div>
+      </div>
+      <div className="game">
+        <img
+          className="tree"
+          src={`./gamification/tree_${level}.png`}
+          onDrop={() => onDrop()}
+        />
+        <div className="inventory">
+          <img
+            className="chest"
+            onClick={() => toggleChest()}
+            src={`./gamification/chest_${chest}.png`}
+          />
+          {chest === "opened" ? (
+            <div className="rewards">
+              <div className="item">
+                <img
+                  value="fertilizer"
+                  draggable="true"
+                  onDrag={(event) => onDrag(event)}
+                  className="img"
+                  src={"./gamification/dirt.png"}
+                />
+                <span>{`${inventory.fertilizer}`}</span>
+              </div>
 
-							<div className="item">
-								<img className="img" src={'./gamification/water.png'} />
-								<span>{`${inventory.water}`}</span>
-							</div>
-						</div>
-					) : null}
-				</div>
-			</div>
-		</div>
-	)
-}
+              <div className="item">
+                <img
+                  draggable="true"
+                  className="img"
+                  src={"./gamification/nutritious_water.png"}
+                />
+                <span>{`${inventory.nutritiousWater}`}</span>
+              </div>
 
-export default PetPlant
+              <div className="item">
+                <img
+                  draggable="true"
+                  className="img"
+                  src={"./gamification/water.png"}
+                />
+                <span>{`${inventory.water}`}</span>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PetPlant;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,63 @@
-const { db } = require('./db')
-const PORT = process.env.PORT || 8080
-const app = require('./app')
-const seed = require('../script/seed');
+const { db } = require("./db");
+const PORT = process.env.PORT || 8080;
+const app = require("./app");
+const seed = require("../script/seed");
+const {
+  models: { User, PetPlant, Posture },
+} = require("./db");
+const cron = require("node-cron");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const init = async () => {
   try {
-    if(process.env.SEED === 'true'){
+    if (process.env.SEED === "true") {
       await seed();
-    }
-    else {
-      await db.sync()
+    } else {
+      await db.sync();
     }
     // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
   } catch (ex) {
-    console.log(ex)
+    console.log(ex);
   }
-}
+};
 
-init()
+init();
+
+cron.schedule("* * * * *", async () => {
+  console.log("this runs every minute");
+  const users = await User.findAll();
+  let today = new Date().toISOString();
+  let date = today.substr(0, 10);
+  for (let i = 0; i < users.length; i++) {
+    let currentUser = users[i];
+    const plant = await PetPlant.findOne({ where: { userId: currentUser.id } });
+    //console.log("plant", plant);
+    const poses = await Posture.findAll({
+      where: {
+        userId: currentUser.id,
+      },
+    });
+    const todaysPoses = poses.filter((pose) => {
+      let currentDate = new Date(pose.createdAt).toISOString();
+      return currentDate.substr(0, 10) == date;
+    });
+    const numOfPoses = todaysPoses.length;
+    const numOfGoodPoses = todaysPoses.filter(
+      (pose) => pose.type === "Good Posture"
+    ).length;
+    const percentageOfGood = (numOfGoodPoses / numOfPoses) * 100;
+
+    if (percentageOfGood >= 90) {
+      plant.inventory.fertilizer++;
+    } else if (percentageOfGood >= 75) {
+      plant.inventory.nutritiousWater++;
+    } else if (percentageOfGood >= 50) {
+      plant.inventory.water++;
+    }
+    // console.log("UPDATED PLANT", plant);
+    const newPlant = await plant.save();
+    //console.log("Saved plant", newPlant);
+  }
+});
