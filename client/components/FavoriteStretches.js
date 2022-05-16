e2a9cd769caec3f94e03cca38192161488818977
Brooke Stenglein <b.morsekarzen@gmail.com>
2022-05-16 14:17:41 -0500

    fixed styling for favorites stretch and stretchlist

--- client/components/FavoriteStretches.js
+++ client/components/FavoriteStretches.js
@@ -1,53 +1,61 @@
-import React, {useEffect, useState} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getFavoriteStretch } from '../store/favoriteList'
-import { Button, Card, Container, Row, Col } from 'react-bootstrap';
-import { deleteFavoriteStretch } from '../store/favoriteList';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getFavoriteStretch } from "../store/favoriteList";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import { deleteFavoriteStretch } from "../store/favoriteList";
 
-export const FavoriteStretches = props => {
+export const FavoriteStretches = (props) => {
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.auth.id);
-  const favoritesArray = useSelector((state) => state.favoriteList)
-  const [stretchToDelete, deleteStretch] = useState({})
-
+  const favoritesArray = useSelector((state) => state.favoriteList);
+  const [stretchToDelete, deleteStretch] = useState({});
 
   useEffect(() => {
     const fetchFavorites = () => {
       if (userId) {
-        dispatch(getFavoriteStretch())
+        dispatch(getFavoriteStretch());
       }
-    }
-    fetchFavorites()
-  },[userId])
+    };
+    fetchFavorites();
+  }, [userId]);
 
-  useEffect(()=> {
-    if(stretchToDelete.id){
-      dispatch(deleteFavoriteStretch(stretchToDelete))
+  useEffect(() => {
+    if (stretchToDelete.id) {
+      dispatch(deleteFavoriteStretch(stretchToDelete));
     }
-  },[stretchToDelete])
+  }, [stretchToDelete]);
 
   return (
-  <div>
-    <Container>
-      <Row>
-      {favoritesArray.length === 0 ? <p>Feel free to add some stretches!</p> : favoritesArray.map(favorite => {
-      const stretch = favorite.stretch
-      return (
-        <Col key={stretch.id}>
-          <Card>
-            <Card.Img variant="top" src={stretch.imageURL} />
-            <Card.Title>{stretch.name}</Card.Title>
-            <Card.Text>{stretch.directions}</Card.Text>
-            <Button onClick={()=> {
-              deleteStretch(stretch)
-            }}>
-              Remove from Favorites</Button>
-          </Card>
-        </Col>
-      )
-    })}
-      </Row>
-    </Container>
-
-  </div>)
-}
+    <div>
+      <h3 id="favorites-title">My Favorites</h3>
+      <Container>
+        <Row>
+          {favoritesArray.length === 0 ? (
+            <p>Feel free to add some stretches!</p>
+          ) : (
+            favoritesArray.map((favorite) => {
+              const stretch = favorite.stretch;
+              return (
+                <Col md={4} key={stretch.id}>
+                  <Card className="favorites-card">
+                    <Card.Img variant="top" src={stretch.imageURL} />
+                    <Card.Title>{stretch.name}</Card.Title>
+                    <Card.Text>{stretch.directions}</Card.Text>
+                    <Button
+                      className="favorites-remove-button"
+                      onClick={() => {
+                        deleteStretch(stretch);
+                      }}
+                    >
+                      Remove from Favorites
+                    </Button>
+                  </Card>
+                </Col>
+              );
+            })
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
+};
--- client/components/StretchList.js
+++ client/components/StretchList.js
@@ -1,45 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Button, Card, Container, Row, Col } from 'react-bootstrap';
-import {addFavoriteStretch} from '../store/favoriteChange'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import { addFavoriteStretch } from "../store/favoriteChange";
 
 export default (props) => {
   const dispatch = useDispatch();
-  const stretchList = useSelector((state) => state.stretchList)
-  const [favorite, setFavorite] = useState({})
+  const stretchList = useSelector((state) => state.stretchList);
+  const [favorite, setFavorite] = useState({});
 
-  useEffect(()=> {
-    if(favorite.id){
-      dispatch(addFavoriteStretch(favorite))
+  useEffect(() => {
+    if (favorite.id) {
+      dispatch(addFavoriteStretch(favorite));
     }
-  },[favorite])
+  }, [favorite]);
 
   return (
     <div>
-      {stretchList.length !== 0 ?
+      {stretchList.length !== 0 ? (
         <div>
           <h2>Suggested Stretches: </h2>
           <Container className="h-50 d-inline-block">
-            <Row >
+            <Row>
               {stretchList.map((stretch) => {
                 return (
-                  <Col xs key={stretch.id} >
-                    <Card >
+                  <Col xs key={stretch.id}>
+                    <Card className="stretchlist-card">
                       <Card.Img variant="top" src={stretch.imageURL} />
                       <Card.Title>{stretch.name}</Card.Title>
                       <Card.Text>{stretch.directions}</Card.Text>
-                      <Button onClick={()=>
-                        {setFavorite(stretch)}
-                        }>Add to Favorites</Button>
+                      <Button
+                        onClick={() => {
+                          setFavorite(stretch);
+                        }}
+                      >
+                        Add to Favorites
+                      </Button>
                     </Card>
                   </Col>
-                )
+                );
               })}
             </Row>
           </Container>
-        </div> : ''}
-        <br />
+        </div>
+      ) : (
+        ""
+      )}
+      <br />
     </div>
-
-  )
-}
+  );
+};
