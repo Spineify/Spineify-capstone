6885f1d67d307188f06d4f416cc1af77a401790d
Brooke Stenglein <b.morsekarzen@gmail.com>
2022-05-16 14:40:56 -0500

    added stretches to seed file

--- script/stretches.js
+++ script/stretches.js
@@ -1,104 +1,127 @@
 let stretches = [
-	{
-		name: 'Forward Stretch',
-		directions:
-			'Clasp your hands in front of you and lower your head in line with your arms. \nPress forward and hold for 10 to 30 seconds.',
-		category: ['upper-back'],
-		imageURL: '/stretches/Forward_Stretch.gif',
-	},
-	{
-		name: 'Neck Stretches',
-		directions:
-			'Relax and lean your head forward. Slowly roll toward one side and hold for 10 seconds. \nRepeat on other side. Relax again and lift your chin back to starting position.',
-		category: ['neck'],
-		imageURL: '/stretches/Neck_Stretch.gif',
-	},
-	{
-		name: 'Upper Trap Stretch',
-		directions:
-			'Gently pull your head toward each shoulder until a light stretch is felt. \nHold the pose for 10 to 15 seconds. \nAlternate once on each side.',
-		category: ['neck'],
-		imageURL: '/stretches/Upper_Trap.gif',
-	},
-	{
-		name: 'Upper Body and Arm Stretch',
-		directions:
-			'Clasp hands together above the head with palms facing outward. \nPush your arms up, stretching upward. \nHold the pose for 10 to 30 seconds.',
-		category: ['upper-back'],
-		imageURL: '/stretches/Upper_Body_Stretch.gif',
-	},
-	{
-		name: 'Triceps Stretches',
-		directions:
-			'Raise your arm and bend it so that your hand reaches toward the opposite side. \nUse your other hand and pull the elbow toward your head. \nHold for 10 to 30 seconds. \nRepeat on the other side.',
-		category: ['upper-back'],
-		imageURL: '/stretches/Tripcep_Stretch.gif',
-	},
-	{
-		name: 'Torso Stretch, or Trunk Rotation',
-		directions:
-			'Keep your feet firmly on the ground, facing forward. \nTwist your upper body in the direction of the arm that’s resting on the back of your chair. \nHold pose for 10 to 30 seconds. \nRepeat on other side.',
-		category: ['hips'],
-		imageURL: '/stretches/Torso_Stretch.gif',
-	},
-	{
-		name: 'Shoulder Shrug',
-		directions:
-			'Raise both shoulders at once up toward the ears. \nDrop them and repeat 10 times each direction.',
-		category: ['shoulders'],
-		imageURL: '/stretches/Shoulder_Shrug.gif',
-	},
-	{
-		name: 'Shoulder, or Pectoralis Stretch',
-		directions:
-			'Clasp hands behind your back. Push the chest outward, and raise the chin. \nHold the pose for 10 to 30 seconds.',
-		category: ['neck'],
-		imageURL: '/stretches/Shoulder_Pec_Stretch.gif',
-	},
-	{
-		name: 'Overhead Reach',
-		directions:
-			'Extend each arm overhead. Reach to the opposite side. \nHold for 10 to 30 seconds. Repeat on the other side.',
-		category: ['upper-back'],
-		imageURL: '/stretches/Overhead_Reach.gif',
-	},
-	{
-		name: "Shoulder Shrugs",
-		directions: `Shrug your shoulders by bringing them up towards your ears and holding for 3-5 seconds. \nRelease and repeat 3-5 times`,
-		category: ["shoulders"],
-		imageURL:
-			'/stretches/wslShoulderShrug.gif',
-	},
-	{
-		name: "Back and Chest Stretch",
-		directions: `Clasping your hands behind your head, squeeze your shoulder blades together.\nHold this squeeze for 5-6 seconds.\nTake a breath, then repeat one more time.`,
-		category: ["upper-back", "shoulders"],
-		imageURL:
-			'/stretches/wslBackandChest.gif',
-	},
-	{
-		name: "Seated Spinal Rotation",
-		directions: `While seated, cross your arms over your chest.\nGrab your shoulders.\nRotate your upper body from the waist, turning gently from left to right as far as feels comfortable.\nYou should feel a tension on both sides of your lower back as it stretches out.`,
-		category: ["lower-back"],
-		imageURL:
-			'/stretches/seated-spinal-rotation.png',
-	},
-	{
-		name: "Neck stretch",
-		directions:
-			"1) Relax in your chair and lean your head forward\n2) Slowly roll toward one side and hold for 10 seconds\n3) Repeat on other side\n4) Relax again and lift your chin back to starting position\n5) Repeat 3 times in each direction",
-		category: ["neck"],
-		imageURL:
-			'/stretches/office_neck_stretch.png',
-	},
-	{
-		name: "Latissimus or Overhead Reach stretch",
-		directions:
-			"1) Extend your right arm overhead and reach to the opposite side\n2) Hold for 10 to 30 seconds\n3) Repeat on the other side using your left arm",
-		category: ["shoulders"],
-		imageURL:
-			'/stretches/office_lat_dorsi_stretch.png',
-	}
-]
+  {
+    name: "Forward Stretch",
+    directions:
+      "Clasp your hands in front of you and lower your head in line with your arms. \nPress forward and hold for 10 to 30 seconds.",
+    category: ["upper-back"],
+    imageURL: "/stretches/Forward_Stretch.gif",
+  },
+  {
+    name: "Neck Stretches",
+    directions:
+      "Relax and lean your head forward. Slowly roll toward one side and hold for 10 seconds. \nRepeat on other side. Relax again and lift your chin back to starting position.",
+    category: ["neck"],
+    imageURL: "/stretches/Neck_Stretch.gif",
+  },
+  {
+    name: "Upper Trap Stretch",
+    directions:
+      "Gently pull your head toward each shoulder until a light stretch is felt. \nHold the pose for 10 to 15 seconds. \nAlternate once on each side.",
+    category: ["neck"],
+    imageURL: "/stretches/Upper_Trap.gif",
+  },
+  {
+    name: "Upper Body and Arm Stretch",
+    directions:
+      "Clasp hands together above the head with palms facing outward. \nPush your arms up, stretching upward. \nHold the pose for 10 to 30 seconds.",
+    category: ["upper-back"],
+    imageURL: "/stretches/Upper_Body_Stretch.gif",
+  },
+  {
+    name: "Triceps Stretches",
+    directions:
+      "Raise your arm and bend it so that your hand reaches toward the opposite side. \nUse your other hand and pull the elbow toward your head. \nHold for 10 to 30 seconds. \nRepeat on the other side.",
+    category: ["upper-back"],
+    imageURL: "/stretches/Tripcep_Stretch.gif",
+  },
+  {
+    name: "Torso Stretch, or Trunk Rotation",
+    directions:
+      "Keep your feet firmly on the ground, facing forward. \nTwist your upper body in the direction of the arm that’s resting on the back of your chair. \nHold pose for 10 to 30 seconds. \nRepeat on other side.",
+    category: ["hips"],
+    imageURL: "/stretches/Torso_Stretch.gif",
+  },
+  {
+    name: "Shoulder Shrug",
+    directions:
+      "Raise both shoulders at once up toward the ears. \nDrop them and repeat 10 times each direction.",
+    category: ["shoulders"],
+    imageURL: "/stretches/Shoulder_Shrug.gif",
+  },
+  {
+    name: "Shoulder, or Pectoralis Stretch",
+    directions:
+      "Clasp hands behind your back. Push the chest outward, and raise the chin. \nHold the pose for 10 to 30 seconds.",
+    category: ["neck"],
+    imageURL: "/stretches/Shoulder_Pec_Stretch.gif",
+  },
+  {
+    name: "Overhead Reach",
+    directions:
+      "Extend each arm overhead. Reach to the opposite side. \nHold for 10 to 30 seconds. Repeat on the other side.",
+    category: ["upper-back"],
+    imageURL: "/stretches/Overhead_Reach.gif",
+  },
+  {
+    name: "Shoulder Shrugs",
+    directions: `Shrug your shoulders by bringing them up towards your ears and holding for 3-5 seconds. \nRelease and repeat 3-5 times`,
+    category: ["shoulders"],
+    imageURL: "/stretches/wslShoulderShrug.gif",
+  },
+  {
+    name: "Shoulder and Chest Stretch",
+    directions: `Clasping your hands behind your head, squeeze your shoulder blades together.\nHold this squeeze for 5-6 seconds.\nTake a breath, then repeat one more time.`,
+    category: ["shoulders"],
+    imageURL: "/stretches/wslBackandChest.gif",
+  },
+  {
+    name: "Seated Spinal Rotation",
+    directions: `While seated, cross your arms over your chest.\nGrab your shoulders.\nRotate your upper body from the waist, turning gently from left to right as far as feels comfortable.\nYou should feel a tension on both sides of your lower back as it stretches out.`,
+    category: ["lower-back"],
+    imageURL: "/stretches/seated-spinal-rotation.png",
+  },
+  {
+    name: "Neck stretch",
+    directions:
+      "Relax in your chair and lean your head forward. \n Slowly roll toward one side and hold for 10 seconds.\n Repeat on other side.\nRelax again and lift your chin back to starting position.\n Repeat 3 times in each direction",
+    category: ["neck"],
+    imageURL: "/stretches/office_neck_stretch.png",
+  },
+  {
+    name: "Latissimus or Overhead Reach stretch",
+    directions:
+      "Extend your right arm overhead and reach to the opposite side.\n Hold for 10 to 30 seconds.\n Repeat on the other side using your left arm",
+    category: ["shoulders"],
+    imageURL: "/stretches/office_lat_dorsi_stretch.png",
+  },
+  {
+    name: "Hip and knee flexion stretch",
+    directions:
+      "Hug one knee at a time, pulling it toward your chest. \nHold the pose for 10 to 30 seconds. \nAlternate.",
+    category: ["hips"],
+    imageURL: "/stretches/HipsStretch.png",
+  },
+  {
+    name: "Hamstrings stretch",
+    directions:
+      "Remaining seated, extend one leg outward. \nReach toward your toes. \nHold for 10 to 30 seconds.",
+    category: ["hips"],
+    imageURL: "/stretches/HamstringStretch.png",
+  },
+  {
+    name: "Shoulder Release",
+    directions:
+      "Stand with your left shoulder next to a wall. \n Extend you left arm straight up, palm facing the wall. \nMove your arm down behind you slowly, as if you were going through each hour on a clock, 12 to 6. \nTurn and repeat with your right arm.",
+    category: ["shoulders"],
+    imageURL: "/stretches/ShoulderRelease.png",
+  },
+  {
+    name: "Seated Forward Flexion",
+    directions:
+      "Begin seated in your chair with feet on the ground at shoulder width. \nKeeping your back straight, bend forward as far as you can. Your arms will hang towards the floor.",
+    category: ["lower-back"],
+    imageURL: "/stretches/ForwardFlexion.png",
+  },
+];
 
-module.exports = stretches
+module.exports = stretches;
