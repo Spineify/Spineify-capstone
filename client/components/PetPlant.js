d7374d479491c5ac7a260d32bdc53e8adc332ca2
Jenny Cho <chojenny112@gmail.com>
2022-05-16 20:57:24 -0400

    game chest conditionals

--- client/components/PetPlant.js
+++ client/components/PetPlant.js
@@ -1,164 +1,179 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getPlant, updatePlant } from '../store/petPlant'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPlant, updatePlant } from "../store/petPlant";
 
 const PetPlant = () => {
-	const [chest, setChest] = useState('closed')
+  const [chest, setChest] = useState("closed");
 
-	//plant data
-	const plant = useSelector((state) => state.plantReducer)
-	const { inventory } = plant
-	const [draggedItem, setDraggedItem] = useState('')
-	const [prevPlant, setPrevPlant] = useState(plant)
-	const dispatch = useDispatch()
+  //plant data
+  const plant = useSelector((state) => state.plantReducer);
+  const { inventory } = plant;
+  const [draggedItem, setDraggedItem] = useState("");
+  const [prevPlant, setPrevPlant] = useState(plant);
+  const dispatch = useDispatch();
 
-	//add setInterval to dispatch(getPlant())
-	//cron job nodejs to schedule and handle notification
-	//notification when received reward/got points deducted
-	useEffect(() => {
-		const getPlantInterval = setInterval(async function () {
-			dispatch(getPlant())
-			const prevInventory = prevPlant.inventory
-			const { inventory } = plant
-			if (
-				prevInventory.fertilizer < inventory.fertilizer ||
-				prevInventory.nutritiousWater < inventory.nutritiousWater ||
-				prevInventory.water < inventory.water
-			) {
-				console.log('RECEIVED MORE PRIZESSSS')
-				electron.notificationApi.sendNotification(
-					`Great Job you got more prizes!`
-				)
-			}
+  const inventoryTotal =
+    inventory.fertilizer + inventory.nutritiousWater + inventory.water;
+  let prizesTotal = inventoryTotal > 0 ? true : false;
 
-			if (prevPlant.points > plant.points) {
-				console.log('LOST POINTTSS')
-				electron.notificationApi.sendNotification(
-					`You haven't fed your tree in a while`
-				)
-			}
+  //add setInterval to dispatch(getPlant())
+  //cron job nodejs to schedule and handle notification
+  //notification when received reward/got points deducted
+  useEffect(() => {
+    const getPlantInterval = setInterval(async function () {
+      dispatch(getPlant());
+      const prevInventory = prevPlant.inventory;
+      const { inventory } = plant;
+      if (
+        prevInventory.fertilizer < inventory.fertilizer ||
+        prevInventory.nutritiousWater < inventory.nutritiousWater ||
+        prevInventory.water < inventory.water
+      ) {
+        console.log("RECEIVED MORE PRIZESSSS");
+        electron.notificationApi.sendNotification(
+          `Great Job you got more prizes!`
+        );
+      }
 
-			setPrevPlant(plant)
-		}, 5000)
+      if (prevPlant.points > plant.points && prevPlant.level === plant.level) {
+        console.log("LOST POINTTSS");
+        electron.notificationApi.sendNotification(
+          `You haven't fed your tree in a while`
+        );
+      }
 
-		return () => {
-			clearInterval(getPlantInterval)
-		}
-	}, [plant])
+      setPrevPlant(plant);
+    }, 5000);
 
-	const onDrag = (event, item) => {
-		event.preventDefault()
-		setDraggedItem(item)
-		console.log('draggedItem onDrag', draggedItem)
-	}
+    return () => {
+      clearInterval(getPlantInterval);
+    };
+  }, [plant]);
 
-	const onDrop = () => {
-		console.log('draggedItem:', draggedItem)
-		dispatch(updatePlant(draggedItem))
-		setDraggedItem('')
-		console.log('updated plant after drop', plant)
+  const onDrag = (event, item) => {
+    event.preventDefault();
+    setDraggedItem(item);
+    console.log("draggedItem onDrag", draggedItem);
+  };
 
-		//add animation when tree is fed
-	}
+  const onDrop = () => {
+    console.log("draggedItem:", draggedItem);
+    dispatch(updatePlant(draggedItem));
+    setDraggedItem("");
+    console.log("updated plant after drop", plant);
 
-	const onDragOver = (event) => {
-		event.preventDefault()
-	}
+    //add animation when tree is fed
+  };
 
-	const level = String(plant.level)
-	const points = String(plant.points)
+  const onDragOver = (event) => {
+    event.preventDefault();
+  };
 
-	//update background based on time of day
-	const date = new Date()
-	const hour = date.getHours()
-	let timeOfDay
-	if (hour > 6 && hour <= 10) {
-		timeOfDay = 'morning'
-	} else if (hour > 10 && hour <= 17) {
-		timeOfDay = 'afternoon'
-	} else {
-		timeOfDay = 'evening'
-	}
+  const level = String(plant.level);
+  const points = String(plant.points);
 
-	//toggle chest animation
-	const toggleChest = () => {
-		if (chest === 'opened') {
-			setChest('closed')
-		} else {
-			setChest('opened')
-		}
-	}
+  //update background based on time of day
+  const date = new Date();
+  const hour = date.getHours();
+  let timeOfDay;
+  if (hour > 6 && hour <= 10) {
+    timeOfDay = "morning";
+  } else if (hour > 10 && hour <= 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "evening";
+  }
 
-	return (
-		<div className={`gameFrame ${timeOfDay}`}>
-			<div className="progress">
-				<div
-					className="progress-bar progress-bar-striped progress-bar-animated"
-					role="progressbar"
-					aria-valuenow={`${points}`}
-					aria-valuemin="0"
-					aria-valuemax="12"
-					style={{ width: `${Math.floor((points / 12) * 100)}%` }}
-				></div>
-			</div>
-			<div className="game">
-				<img
-					className="tree"
-					src={`./gamification/tree_${level}.png`}
-					onDrop={() => onDrop()}
-					onDragOver={(event) => onDragOver(event)}
-				/>
-				<div className="inventory">
-					<img
-						className="chest"
-						onClick={() => toggleChest()}
-						src={`./gamification/chest_${chest}.png`}
-					/>
-					{chest === 'opened' ? (
-						//if inventory is empty, show nothing or send message
-						<div className="rewards">
-							{/* if inventory of item is 0, dont render */}
-							<div className="item">
-								<img
-									value="fertilizer"
-									draggable="true"
-									onDrag={(event) => onDrag(event, 'fertilizer')}
-									className="img"
-									src={'./gamification/dirt.png'}
-								/>
-								<span>{`${inventory.fertilizer}`}</span>
-							</div>
+  //toggle chest animation
+  const toggleChest = () => {
+    if (chest === "opened") {
+      setChest("closed");
+    } else {
+      setChest("opened");
+    }
+  };
 
-							{/* if inventory of item is 0, dont render */}
-							<div className="item">
-								<img
-									value="nutritiousWater"
-									draggable="true"
-									onDrag={(event) => onDrag(event, 'nutritiousWater')}
-									className="img"
-									src={'./gamification/nutritious_water.png'}
-								/>
-								<span>{`${inventory.nutritiousWater}`}</span>
-							</div>
+  return (
+    <div className={`gameFrame ${timeOfDay}`}>
+      <div className="progress">
+        <div
+          className="progress-bar progress-bar-striped progress-bar-animated"
+          role="progressbar"
+          aria-valuenow={`${points}`}
+          aria-valuemin="0"
+          aria-valuemax="12"
+          style={{ width: `${Math.floor((points / 12) * 100)}%` }}
+        ></div>
+      </div>
+      <div className="game">
+        <img
+          className="tree"
+          src={`./gamification/tree_${level}.png`}
+          onDrop={() => onDrop()}
+          onDragOver={(event) => onDragOver(event)}
+        />
+        <div className="inventory">
+          <img
+            className="chest"
+            onClick={() => toggleChest()}
+            src={`./gamification/chest_${chest}_new.png`}
+          />
+          {chest === "opened" && prizesTotal ? (
+            //if inventory is empty, show nothing or send message
+            <div className="rewards">
+              {/* if inventory of item is 0, dont render */}
+              {inventory.fertilizer > 0 && (
+                <div className="item">
+                  <img
+                    value="fertilizer"
+                    draggable="true"
+                    onDrag={(event) => onDrag(event, "fertilizer")}
+                    className="img"
+                    src={"./gamification/dirt.png"}
+                  />
+                  <span>{`${inventory.fertilizer}`}</span>
+                </div>
+              )}
+              {/* if inventory of item is 0, dont render */}
+              {inventory.nutritiousWater > 0 && (
+                <div className="item">
+                  <img
+                    value="nutritiousWater"
+                    draggable="true"
+                    onDrag={(event) => onDrag(event, "nutritiousWater")}
+                    className="img"
+                    src={"./gamification/nutritious_water.png"}
+                  />
+                  <span>{`${inventory.nutritiousWater}`}</span>
+                </div>
+              )}
 
-							{/* if inventory of item is 0, dont render */}
-							<div className="item">
-								<img
-									value="water"
-									draggable="true"
-									onDrag={(event) => onDrag(event, 'water')}
-									className="img"
-									src={'./gamification/water.png'}
-								/>
+              {/* if inventory of item is 0, dont render */}
+              {inventory.water > 0 && (
+                <div className="item">
+                  <img
+                    value="water"
+                    draggable="true"
+                    onDrag={(event) => onDrag(event, "water")}
+                    className="img"
+                    src={"./gamification/water.png"}
+                  />
 
-								<span>{`${inventory.water}`}</span>
-							</div>
-						</div>
-					) : null}
-				</div>
-			</div>
-		</div>
-	)
-}
+                  <span>{`${inventory.water}`}</span>
+                </div>
+              )}
+            </div>
+          ) : null}
+          {chest === "opened" && !prizesTotal ? (
+            //if inventory is empty, show nothing or send message
+            <p className="bubble">
+              No prizes in your chest! Maintain Good posture to earn more~
+            </p>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PetPlant
+export default PetPlant;
